35619748f5cadf6e8d6b51ef1053d35e820e5f76
Md-Hazrat <mdhazratali8866@gmail.com>
2024-10-30 19:04:07 +0600

    connect with mongoose

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("./db/config");
+const User = require("./db/User");
+const app = express();
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+  const result = await user.save();
+  res.send(result);
+});
+
+app.listen(5000);
