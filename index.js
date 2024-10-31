7a4da91c9065ad12ae7f069161288235004aabd6
Md-Hazrat <mdhazratali8866@gmail.com>
2024-10-31 16:43:25 +0600

    some modify

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 require("./db/config");
 const User = require("./db/User");
 const app = express();
+const cors = require("cors");
 app.use(express.json());
+app.use(cors());
 
 app.post("/register", async (req, res) => {
   const user = new User(req.body);
